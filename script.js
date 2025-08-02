a095965de5975d6132a480c25f8aae0286a501e6
Mixtyc1 <victor.martins56763@gmail.com>
2025-08-02 03:44:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+function abrirCarta() {
+  document.querySelector(".tampa").style.transform = "rotateX(-120deg)";
+  document.querySelector(".mensagem").style.display = "block";
+  setTimeout(() => {
+    document.querySelector(".carta").style.opacity = 1;
+  }, 1000);
+  setInterval(criarCoracao, 1000);
+}
+
 function criarCoracao() {
   const coracao = document.createElement('div');
   coracao.classList.add('coração');
   coracao.innerText = '❤';
   coracao.style.left = Math.random() * window.innerWidth + 'px';
-  coracao.style.top = '100%';
+  coracao.style.top = window.innerHeight + 'px';
   document.body.appendChild(coracao);
 
   setTimeout(() => {
     coracao.remove();
   }, 5000);
 }
-
-setInterval(criarCoracao, 800);
